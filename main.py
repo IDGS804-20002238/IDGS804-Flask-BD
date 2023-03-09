14136a24f8b9eb4afc5255577dc2f18e2d082362
Angel Tovar <angeltovar308@gmail.com>
2023-03-08 19:36:33 -0600

    clase 08-03-2023

--- main.py
+++ main.py
@@ -18,10 +18,51 @@ def index():
                      email=create_form.email.data)
         db.session.add(alum)
         db.session.commit()
+        return redirect(url_for('ABCompleto'))
     return render_template('index.html', form=create_form)
 
+@app.route("/ABCompleto", methods=['GET','POST'])
+def ABCompleto():
+    create_form=forms.UserForm(request.form)
 
+    #select * from alumnos
+    alumnos=Alumnos.query.all()
+    return render_template('ABCompleto.html', form=create_form,alumnos=alumnos)
 
+@app.route("/modificar", methods=['GET','POST'])
+def modificar():
+    create_form=forms.UserForm(request.form)
+    if request.method=='GET':
+        id=request.args.get('id')
+        #select from * alumnos wher id=id
+        alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        create_form.id.data=request.args.get('id')
+        create_form.nombre.data=alum1.nombre
+        create_form.apaterno.data=alum1.apaterno
+        create_form.email.data=alum1.email
+
+    if request.method=='POST':
+        id=create_form.id.data
+        alum=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        alum.nombre=create_form.nombre.data
+        alum.apaterno=create_form.apaterno.data
+        alum.email=create_form.email.data
+        db.session.add(alum)
+        db.session.commit()
+        return redirect(url_for('ABCompleto'))
+    return render_template('modificar.html',form=create_form)
+
+@app.route("/eliminar", methods=['GET', 'POST'])
+def eliminar():
+    id=request.args.get('id')
+    alum = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+
+    if request.method == 'POST':
+        db.session.delete(alum)
+        db.session.commit()
+        return redirect(url_for('ABCompleto'))
+
+    return render_template('eliminar.html', alumno=alum)
 
 
 if __name__ == '__main__':
